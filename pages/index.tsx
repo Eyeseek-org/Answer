7450dc3eeab55ba7dbe47be6cbadaa3ce9faab52
kazii <kazdan@seznam.cz>
2022-11-05 18:28:07 +0100

    Categories migrated to latest projects

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,9 @@ import Head from "next/head";
 import Image from "next/image";
 import styled from "styled-components";
 import Eye7 from "../public/Eye7.png";
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { useEffect } from "react";
 import Footer from "../sections/Footer";
-import {cats} from '../data/cats'
+
 import LandingMain from "../sections/Landing/LandingMain";
 import LatestProjects from "../sections/Landing/LatestProjects";
 import Features from "../sections/Landing/Features";
@@ -25,71 +24,12 @@ const EyeSevenBox = styled.div`
   position: relative;
 `;
 
-const Categories = styled.div`
-    display: flex;
-    flex-direction: row;
-    border-top: 1px solid #e6e6e6;
-    border-bottom: 1px solid #e6e6e6;
-    padding-top: 20px;
-    padding-bottom: 20px;
-    padding-left: 5%;
-    gap: 4%;
-    justify-content: center;
-`
-
-const Cat = styled.div`
-    font-size: 1em;
-    font-family: 'Montserrat';
-    &:hover{
-        cursor: pointer;
-        opacity: 0.9;
-    }
-`
-
-const ACat = styled(Cat)`
-    color: white;
-`
-
-
 const Home: NextPage = () => {
-  const initialCategory = "All"
-  const [projects, setProjects] = useState([]);
-  const [category, setCategory] = useState(initialCategory)
-
 
   useEffect(() => {
     window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
-    getProjects();
   }, []);
 
-  const getProjects = async (cat?) => {
-    const config = {
-      headers: {
-        "X-Parse-Application-Id": `${process.env.NEXT_PUBLIC_DAPP_ID}`,
-      },
-    };
-    const selectedCategory = cat ? cat : category
-    try {
-      if (selectedCategory === "All") {
-        const res = await axios.get(`${process.env.NEXT_PUBLIC_DAPP}/classes/Project?where={"state":1}`, config);
-        setProjects(res.data.results);
-      } else {
-        const res = await axios.get(`${process.env.NEXT_PUBLIC_DAPP}/classes/Project?where={"category":"${selectedCategory}", "state": 1}`, config);
-        setProjects(res.data.results);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const handleCat = async(cat) => {
-    try {
-      await setCategory(cat)
-      await getProjects(cat)
-    } catch(err) {
-        console.log(err)
-      }
-  }
 
 
   return (
@@ -103,12 +43,7 @@ const Home: NextPage = () => {
         <LandingMain width={'100%'} height={'100%'}/>
         {/* <ImageBox><Image src={Eye1} alt='Eye1' width={'1000px'} /></ImageBox> */}
         <Features />
-        <Categories>
-            {cats.map((cat) =>
-             <div key={cat}>{cat === category ? <Cat onClick={()=>handleCat(initialCategory)}>{cat}</Cat> : <ACat  onClick={()=>{handleCat(cat)}}>{cat}</ACat>}</div>
-        )}
-        </Categories>
-        <LatestProjects data={projects} my={false}/>
+        <LatestProjects my={false}/>
         <EyeSevenBox>
         <Image src={Eye7} alt="Eye7" width={"350px"} height={"30px"} />
         </EyeSevenBox>
--- sections/Landing/LatestProjects.js
+++ sections/Landing/LatestProjects.js
@@ -1,7 +1,12 @@
 import styled from 'styled-components'
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 import ProjectCard from '../../components/cards/ProjectCard'
 import SectionTitle from '../../components/typography/SectionTitle'
 import Carousel from 'nuka-carousel/lib/carousel'
+import {cats} from '../../data/cats'
+import { moralisApiConfig } from '../../data/moralisApiConfig';
 
 const Container = styled.div`
     margin-top: 5%;
@@ -15,14 +20,82 @@ const ProjectBox = styled.div`
     padding: 5%;
     gap: 2%;
 `
+
+const Categories = styled.div`
+    display: flex;
+    flex-direction: row;
+    border-top: 1px solid #e6e6e6;
+    border-bottom: 1px solid #e6e6e6;
+    padding-top: 20px;
+    padding-bottom: 20px;
+    padding-left: 5%;
+    margin-top: 5%;
+    margin-bottom: 1%;
+    gap: 4%;
+    justify-content: center;
+`
+
+const Cat = styled.div`
+    font-size: 1em;
+    font-family: 'Montserrat';
+    &:hover{
+        cursor: pointer;
+        opacity: 0.9;
+    }
+`
+
+const ACat = styled(Cat)`
+    color: #B0F6FF;
+`
+
 /// @notice "my" indicates whether component visualized in context of MyProjects or Landing page
-const LatestProjects = ({ data, my}) => {
+const LatestProjects = ({ my}) => {
+    const initialCategory = "All"
+    const [data, setData] = useState([]);
+    const [category, setCategory] = useState(initialCategory)
+  
+  
+    useEffect(() => {
+      getProjects();
+    }, []);
+  
+
+    const getProjects = async (cat) => {
+        const selectedCategory = cat ? cat : category
+        try {
+          if (selectedCategory === "All") {
+            const res = await axios.get(`${process.env.NEXT_PUBLIC_DAPP}/classes/Project?where={"state":1}`, moralisApiConfig);
+            setData(res.data.results);
+          } else {
+            const res = await axios.get(`${process.env.NEXT_PUBLIC_DAPP}/classes/Project?where={"category":"${selectedCategory}", "state": 1}`, moralisApiConfig);
+            setData(res.data.results);
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      };
+    
+      const handleCat = async(cat) => {
+        try {
+          await setCategory(cat)
+          await getProjects(cat)
+        } catch(err) {
+            console.log(err)
+          }
+      }
+    
 
     const filteredFirst = data.slice(0, 6)
     const filteredSecond = data.slice(7, 13)
     const filteredThird = data.slice(14, 20)
     return <Container>
         {my ? <SectionTitle title='Project history' subtitle='Looking back at your success' /> : <SectionTitle title='Recently added' subtitle={'Support latest projects'} />}
+        <Categories>
+            {category === initialCategory ? <ACat onClick={() => handleCat(initialCategory)}>All</ACat> : <Cat onClick={() => handleCat(initialCategory)}>All</Cat>}
+            {cats.map((cat) =>
+             <div key={cat}>{cat === category ? <ACat onClick={()=>handleCat(initialCategory)}>{cat}</ACat> : <Cat  onClick={()=>{handleCat(cat)}}>{cat}</Cat>}</div>
+        )}
+        </Categories>
         <Carousel>
             <ProjectBox>
                     {filteredFirst.map((project, index) => {
