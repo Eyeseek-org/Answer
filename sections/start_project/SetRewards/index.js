62e77bf21cbf16e23314e5ceadd1d57f33e00448
Pes <pes@email.com>
2022-11-06 11:03:45 +0100

    Project creation - add multiple rewards, rewards saved to appState & DB

--- sections/start_project/SetRewards/index.js
+++ sections/start_project/SetRewards/index.js
@@ -1,19 +1,19 @@
-import { useState } from "react";
-import { useApp } from "../../utils/appContext";
+import {useState} from "react";
+import {useApp} from "../../utils/appContext";
 
 import SectionTitle from "../../../components/typography/SectionTitle";
-import { RewardContainer, ButtonRow, TabRow, TooltipBox, IconBox,RewardDesc, TokenTooltip } from "./StyleWrapper";
-import { MainContainer, NextButton } from "../Category/StyleWrapper";
+import {ButtonRow, IconBox, RewardContainer, RewardDesc, TabRow, TokenTooltip, TooltipBox} from "./StyleWrapper";
+import {MainContainer, NextButton} from "../Category/StyleWrapper";
 import InputContainer from "../../../components/form/InputContainer";
 import Tab from "../../../components/form/Tab";
-import { MainMilestoneContainer, MilestoneContainer  } from "../SetGoals/StyleWrapper";
+import {MainMilestoneContainer, MilestoneContainer} from "../SetGoals/StyleWrapper";
 import Tooltip from "../../../components/Tooltip";
-import { InfoIcon } from "../../../components/icons/Common";
-import { Row } from "../../../components/format/Row";
+import {InfoIcon} from "../../../components/icons/Common";
+import {Row} from "../../../components/format/Row";
 
 const SetRewards = ({ setStep }) => {
   const { appState, setAppState } = useApp();
-  const { pType, rewards } = { ...appState };
+  const { pType } = { ...appState };
   const [rType, setRType] = useState('Direct donate')
   const [microTooltip, setMicroTooltip] = useState(false)
   const [tokenTooltip, setTokenTooltip] = useState(false)
@@ -22,33 +22,41 @@ const SetRewards = ({ setStep }) => {
     name: "EYE",
     address: process.env.NEXT_PUBLIC_AD_TOKEN,
   })
-  const [reward, setReward] = useState({
-    title: "Reward 1",
-    description: "Reward 1 description",
-    amount: 100,
-    type: { rType },  // OR Donate OR Stream 
-  })
+  const [rewards, setRewards] = useState([])
+  const [rewardsCounts, setRewardsCount] = useState(0);
   const [donationTooltip, setDonationTooltip] = useState(false)
   const [showToken,setShowToken] = useState(false)
 
-  const [showReward, setShowReward] = useState(false)
-
-
   const handleClick = () => {
     setStep((prev) => (prev += 1));
-    setAppState((prev) => ({ ...prev, tokenReward: tReward}))
-    // addReward()
+    setAppState((prev) => ({ ...prev, tokenReward: tReward, rewards: rewards}))
   };
 
   const handleBack = () => {
     setStep((prev) => (prev -= 1));
   }
 
-  const addReward = () => {
-    const newReward = [...rewards, reward];
-    // TBD new reward is added to the app state
+  const handleRemoveReward = () => {
+    setRewardsCount(rewardsCounts - 1);
+    const newRewards = rewards;
+    newRewards.pop();
+    setRewards(newRewards);
   }
-  /// TBD possibility to add multiple rewards, similar as `SetGoals`, and remove
+
+  const handleChangeReward = (rewardIndex, title, desc, amount, cap) => {
+    const newRewards = rewards;
+    let newReward = newRewards[rewardIndex];
+    if (!newReward) {
+        newReward = {};
+    }
+    newReward.title = title ?? newReward.title;
+    newReward.description = desc ?? newReward.description;
+    newReward.amount = amount ?? newReward.amount;
+    newReward.cap = cap ?? newReward.cap;
+    newRewards.splice(rewardIndex, 1, newReward);
+    setRewards(newRewards);
+  }
+
   // TBD Token URL validate HTTPS - https://polygonscan.com/token/0x2791bca1f2de4661ed88a30c99a7a9449aa84174
   // Add to my projects
   //TBD - Object is override with each change, function is needed to fix
@@ -57,74 +65,76 @@ const SetRewards = ({ setStep }) => {
     <SectionTitle title='Offer rewards for backers' subtitle='' />
     <MainContainer>
       <RewardContainer>
-        <NextButton onClick={() => setShowReward(!showReward)}>{showReward ? <>Remove reward</> : <>Add reward (optional)</>}</NextButton>
-        {showReward && <MainMilestoneContainer>
-          <MilestoneContainer>
-            <TabRow> {pType === 'Standard' && 
-              <Tab  
-                active={rType} 
-                o1={'Microfund'} 
-                o2={'Direct donate'} 
-                change1={() =>{ setRType('Microfund') }} 
-                change2={() =>{ setRType('Direct donate') }} />}
-              
-              <TooltipBox>
-                {microTooltip && <Tooltip text={'Microfund creators will get rewards for setting specific maximum cap, even though total amount does not have to be completely transferred to your project at the end. Higher number of microfunds positively impacts following donations.'} />}
-                {donationTooltip && <Tooltip text={'Fixed pledge given by direct donation. Standard Kickstarter-like backing experience with no extra magic around. With reward for direct donation backer knows for certain, how much value will be spend at the end for this reward.'} />}
-              </TooltipBox>
-              <RewardDesc>
-                {rType === 'Direct donate' && <>Offer rewards to the backers on your own responsibility and effort. Eyeseek will provide addresses of backers, rest is on you.</>}
-                {rType === 'Microfund' && <>Offer rewards to the microfund deployers for they intention to incentivize the crowdfunding chain reaction.</>}
-                
-                </RewardDesc>
-            </TabRow>
-            <InputContainer
-              label={'Title'}
-              placeholder={'Godspeed'}
-              description={'Create a unique title for your reward'}
-              onChange={(e)=>{setReward({reward: {title: e.target.value}})}}
-              type={'textArea'}
-            />
-            <InputContainer
-              label={'Description'}
-              placeholder={'Backer receives autographed copy of the book'}
-              description={'Describe what backer receives for this reward'}
-              onChange={(e)=>{setReward({reward: {description: e.target.value}})}}
-              type={'text'}
-            />
-          {rType ? <InputContainer
-              label={'Amount'}
-              placeholder={'1000'}
-              onChange={(e)=>{setReward({reward: {amount: e.target.value}})}}
-              description={
-                <Row>Required microfund cap pledge
-                  <IconBox onMouseEnter={() => setMicroTooltip(true)} onMouseLeave={() => setMicroTooltip(false)}>
-                    <InfoIcon width={15} />
-                  </IconBox>
-                </Row>}
-              type={'number'}
-            />
-            : <InputContainer
-              label={'Amount'}
-              placeholder={'1000'}
-              onChange={(e)=>{setReward({reward: {amount: e.target.value}})}}
-              description={
-                <Row>Backed amount
-                  <IconBox onMouseEnter={() => setDonationTooltip(true)} onMouseLeave={() => setDonationTooltip(false)}>
-                    <InfoIcon width={15} />
-                  </IconBox>
-                </Row>}
-              type={'number'}
-            />}
-            <InputContainer
-              label={'Number'}
-              placeholder={'10'}
-              description={'Maximum number of offered rewards'}
-              onChange={(e)=>{setReward({reward: {cap: e.target.value}})}}
-              type={'number'}
-            />
-          </MilestoneContainer>
-        </MainMilestoneContainer>}
+        <NextButton onClick={() => setRewardsCount(rewardsCounts + 1)}>Add reward (optional)</NextButton>
+        {rewardsCounts > 0 && <NextButton onClick={() => handleRemoveReward()}>Remove last reward</NextButton>}
+        {[...Array(rewardsCounts).keys()].map((rewardIndex) => (
+            <MainMilestoneContainer>
+              <MilestoneContainer>
+                <TabRow> {pType === 'Standard' &&
+                  <Tab
+                    active={rType}
+                    o1={'Microfund'}
+                    o2={'Direct donate'}
+                    change1={() =>{ setRType('Microfund') }}
+                    change2={() =>{ setRType('Direct donate') }} />}
+
+                  <TooltipBox>
+                    {microTooltip && <Tooltip text={'Microfund creators will get rewards for setting specific maximum cap, even though total amount does not have to be completely transferred to your project at the end. Higher number of microfunds positively impacts following donations.'} />}
+                    {donationTooltip && <Tooltip text={'Fixed pledge given by direct donation. Standard Kickstarter-like backing experience with no extra magic around. With reward for direct donation backer knows for certain, how much value will be spend at the end for this reward.'} />}
+                  </TooltipBox>
+                  <RewardDesc>
+                    {rType === 'Direct donate' && <>Offer rewards to the backers on your own responsibility and effort. Eyeseek will provide addresses of backers, rest is on you.</>}
+                    {rType === 'Microfund' && <>Offer rewards to the microfund deployers for they intention to incentivize the crowdfunding chain reaction.</>}
+
+                    </RewardDesc>
+                </TabRow>
+                <InputContainer
+                  label={'Title'}
+                  placeholder={'Godspeed'}
+                  description={'Create a unique title for your reward'}
+                  onChange={(e)=>{handleChangeReward(rewardIndex, e.target.value, undefined, undefined, undefined)}}
+                  type={'textArea'}
+                />
+                <InputContainer
+                  label={'Description'}
+                  placeholder={'Backer receives autographed copy of the book'}
+                  description={'Describe what backer receives for this reward'}
+                  onChange={(e)=>{handleChangeReward(rewardIndex, undefined, e.target.value, undefined, undefined)}}
+                  type={'text'}
+                />
+              {rType ? <InputContainer
+                  label={'Amount'}
+                  placeholder={'1000'}
+                  onChange={(e)=>{handleChangeReward(rewardIndex, undefined, undefined, e.target.value, undefined)}}
+                  description={
+                    <Row>Required microfund cap pledge
+                      <IconBox onMouseEnter={() => setMicroTooltip(true)} onMouseLeave={() => setMicroTooltip(false)}>
+                        <InfoIcon width={15} />
+                      </IconBox>
+                    </Row>}
+                  type={'number'}
+                />
+                : <InputContainer
+                  label={'Amount'}
+                  placeholder={'1000'}
+                  onChange={(e)=>{handleChangeReward(rewardIndex, undefined, undefined, e.target.value, undefined)}}
+                  description={
+                    <Row>Backed amount
+                      <IconBox onMouseEnter={() => setDonationTooltip(true)} onMouseLeave={() => setDonationTooltip(false)}>
+                        <InfoIcon width={15} />
+                      </IconBox>
+                    </Row>}
+                  type={'number'}
+                />}
+                <InputContainer
+                  label={'Number'}
+                  placeholder={'10'}
+                  description={'Maximum number of offered rewards'}
+                  onChange={(e)=>{handleChangeReward(rewardIndex, undefined, undefined, undefined, e.target.value)}}
+                  type={'number'}
+                />
+              </MilestoneContainer>
+          </MainMilestoneContainer>))}
       {pType !== 'Stream' ? <><NextButton onClick={() => setShowToken(!showToken)}>
         {!showToken ?  <>Token reward (optional)</> : <>Remove token reward</>}
         </NextButton>
